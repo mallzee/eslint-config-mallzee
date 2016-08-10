91c10e2abace3c6f3e6ff730afed6b3f3f7d0cf1
Jamie Sutherland <jamie@mallzee.com>
2016-08-10 17:00:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [],
+  rules: {
+    'func-names': 'off',
+    'padded-blocks': 'off',
+    'comma-dangle': 'off',
+    'no-multi-spaces': 'off',
+
+    // doesn't work in node v4 :(
+    strict: 'off',
+    'prefer-rest-params': 'off'
+  },
+  env: {
+    mocha: true
+  }
+};
